7af6ca26406e54909b752eaf93aa3e15f658a964
MerkhatM <merhatmanlaev@gmail.com>
2023-04-09 23:08:00 +0600

    total balance

--- src/components/CostCard/CostCard.jsx
+++ src/components/CostCard/CostCard.jsx
@@ -4,6 +4,9 @@ import food from  "../../assets/food.png";
 import transport from "../../assets/transportpng.png";
 import purchase from "../../assets/buy.png";
 const CostCard = () => {
+    function sumBalance(cards) {
+        return cards.reduce((total, card) => total + parseFloat(card.price), 0);
+    }
     const initialCardBlocks = [
         { title: 'Еда', img: food, price: 5000 },
         { title: 'Транспорт', img: transport, price: 5300 },
@@ -14,7 +17,7 @@ const CostCard = () => {
     const addCardBlock = () => {
         setCardBlocks([
             ...cardBlocks,
-            { title: 'Покупки', img: 'https://w7.pngwing.com/pngs/34/771/png-transparent-computer-icons-retail-supermarket-shopping-retail-white-text-rectangle.png', price: 'Balance' },
+            { title: 'Покупки', img: 'https://w7.pngwing.com/pngs/34/771/png-transparent-computer-icons-retail-supermarket-shopping-retail-white-text-rectangle.png', price: 50000 },
         ]);
     };
 
@@ -22,14 +25,14 @@ const CostCard = () => {
         <div className="card">
             <div className="header">
                 <h2>Cost</h2>
-                <p>$500</p>
+                <p>{sumBalance(cardBlocks)} KZT</p>
             </div>
             <div className="body">
                 {cardBlocks.map((block, index) => (
                     <div key={index} className="card-block">
                         <h3>{block.title}</h3>
                         <img src={block.img} alt={block.title} />
-                        <span>{block.price}</span>
+                        <span>{block.price} KZT</span>
                     </div>
                 ))}
                 <button onClick={addCardBlock}>+</button>
--- src/components/IncomeCard/IncomeCard.jsx
+++ src/components/IncomeCard/IncomeCard.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import "./style.css";
 const IncomeCard = () => {
-
+    function sumBalance(cards) {
+        return cards.reduce((total, card) => total + parseFloat(card.price), 0);
+    }
     const [cardBlocks, setCardBlocks] = useState([]);
 
     const addCardBlock = () => {
         setCardBlocks([
             ...cardBlocks,
-            {title: 'МУИТ', img: 'https://itest.kz/uploads/contest/partners/iitu.jpg', text: 'balance'},
+            {title: 'МУИТ', img: 'https://itest.kz/uploads/contest/partners/iitu.jpg', price:36000},
 
         ]);
     };
@@ -16,14 +18,14 @@ const IncomeCard = () => {
         <div className="card">
             <div className="header">
                 <h2>Income</h2>
-                <p>$1000</p>
+                <p>{sumBalance(cardBlocks)} KZT</p>
             </div>
             <div className="body">
                 {cardBlocks.map((block, index) => (
                     <div key={index} className="card-block">
                         <h3>{block.title}</h3>
                         <img src={block.img} alt={block.title} />
-                        <span>{block.text}</span>
+                        <span>{block.price}</span>
                     </div>
                 ))}
                 <button onClick={addCardBlock}>+</button>
