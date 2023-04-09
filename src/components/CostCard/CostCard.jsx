be9d36892eb118c2753cf110acd4fdc695490f1e
MerkhatM <merhatmanlaev@gmail.com>
2023-04-09 15:18:30 +0600

    прокрутка карточек

--- src/components/CostCard/CostCard.jsx
+++ src/components/CostCard/CostCard.jsx
@@ -5,16 +5,16 @@ import transport from "../../assets/transportpng.png";
 import purchase from "../../assets/buy.png";
 const CostCard = () => {
     const initialCardBlocks = [
-        { title: 'Еда', img: food, text: 5000 },
-        { title: 'Транспорт', img: transport, text: 5300 },
-        { title: 'Покупки', img: purchase, text: 100000 },
+        { title: 'Еда', img: food, price: 5000 },
+        { title: 'Транспорт', img: transport, price: 5300 },
+        { title: 'Покупки', img: purchase, price: 100000 },
     ];
     const [cardBlocks, setCardBlocks] = useState(initialCardBlocks);
 
     const addCardBlock = () => {
         setCardBlocks([
             ...cardBlocks,
-            { title: 'Покупки', img: 'https://w7.pngwing.com/pngs/34/771/png-transparent-computer-icons-retail-supermarket-shopping-retail-white-text-rectangle.png', text: 'Balance' },
+            { title: 'Покупки', img: 'https://w7.pngwing.com/pngs/34/771/png-transparent-computer-icons-retail-supermarket-shopping-retail-white-text-rectangle.png', price: 'Balance' },
         ]);
     };
 
@@ -29,7 +29,7 @@ const CostCard = () => {
                     <div key={index} className="card-block">
                         <h3>{block.title}</h3>
                         <img src={block.img} alt={block.title} />
-                        <span>{block.text}</span>
+                        <span>{block.price}</span>
                     </div>
                 ))}
                 <button onClick={addCardBlock}>+</button>
